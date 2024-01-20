4509acc27922f8956acfc24c226902bc44f1d380
Miguel Leonar <migsldev@gmail.com>
2024-01-20 15:31:33 +1100

    modified files and refactored code to become shorter

--- dice.js
+++ dice.js
@@ -1,88 +1,39 @@
-document.addEventListener("DOMContentLoaded", diceGame)
-
-const diceImage1 = document.querySelector(".img1");
-const diceImage2 = document.querySelector(".img2");
-
-function getDiceNumber() {
-        return Math.floor(Math.random() * 6 + 1); // generates random number from 1 to 6
-    }
 function diceGame() {
+    //Player 1 dice
+    const randomNumber1 = Math.floor(Math.random() * 6 + 1); // creates a random number
 
-    function setDiceImage1() { //function to change dice image depending on getDiceNumber()
-        const randomNumber1 = getDiceNumber();
-    
-        if (randomNumber1 === 1) {
-            diceImage1.setAttribute("src","./images/dice1.png");
-            
-        } 
-        else if (randomNumber1 === 2) {
-            diceImage1.setAttribute("src","./images/dice2.png");    
-            
-        }
-        else if (randomNumber1 === 3) {
-            diceImage1.setAttribute("src","./images/dice3.png");    
-            
-        }
-        else if (randomNumber1 === 4) {
-            diceImage1.setAttribute("src","./images/dice4.png");    
-            
-        }
-        else if (randomNumber1 === 5) {
-            diceImage1.setAttribute("src","./images/dice5.png");    
-               
-        }
-        else if (randomNumber1 === 6) {
-            diceImage1.setAttribute("src","./images/dice6.png");    
-            
-        }
-    }
-        
-    function setDiceImage2() { //function to change dice image depending on getDiceNumber()
-        const randomNumber1 = getDiceNumber();
-    
-        if (randomNumber1 === 1) {
-            
-            diceImage2.setAttribute("src","./images/dice1.png");
-        } 
-        else if (randomNumber1 === 2) {
-            
-            diceImage2.setAttribute("src","./images/dice2.png");    
-        }
-        else if (randomNumber1 === 3) {
-             
-            diceImage2.setAttribute("src","./images/dice3.png");    
-        }
-        else if (randomNumber1 === 4) {
-              
-            diceImage2.setAttribute("src","./images/dice4.png");    
-        }
-        else if (randomNumber1 === 5) {
-              
-            diceImage2.setAttribute("src","./images/dice5.png");    
-        }
-        else if (randomNumber1 === 6) {
-            
-            diceImage2.setAttribute("src","./images/dice6.png");    
-        }
-        
-    }
-    let value1 = Number(setDiceImage1());
-    let value2 = Number(setDiceImage2());
+    const randomImageSource1 = `./images/dice${randomNumber1}.png`
 
-    function displayWinnerDrawLoser() {
+    document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+    //Player 2 dice
+
+    const randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+    const randomImageSource2 = `./images/dice${randomNumber2}.png`
+
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+    //Changing Header(h1) to Winner, Draw
+
+    function displayWinnerDraw() {
         const display = document.querySelector("h1");
 
-        if(value1 > value2) {
+        if(randomNumber1 > randomNumber2) {
             display.innerHTML = "PLAYER 1 WINS!";
         }
-        else if(value1 < value2) {
+        else if(randomNumber1 < randomNumber2) {
             display.innerHTML = "PLAYER 2 WINS!";
         }
-        else if (value1 === value2) {
+        else if (randomNumber1 === randomNumber2) {
             display.innerHTML = "DRAW";
         }
     }
-    displayWinnerDrawLoser();
+    displayWinnerDraw();
 }
+
+document.getElementById("playButton").addEventListener("click", function() {
+    location.reload();
+});
 
 diceGame();
